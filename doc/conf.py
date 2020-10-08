5a51ab1d5e39027130eb012a809ac797b0a2683a
viviane.marty <viviane@seervision.com>
2020-10-08 02:44:56 +0200

    changed the html theme to get rid of the imports

--- doc/conf.py
+++ doc/conf.py
@@ -51,9 +51,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_glpi_theme
-html_theme = "glpi"
-html_theme_path = sphinx_glpi_theme.get_html_themes_path()
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
